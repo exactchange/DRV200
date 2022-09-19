86371fb4138da5acfe53b7780195cb6e79f64d05
exactchange <benny@exactchange.network>
2022-09-19 11:19:42 -0700

    specify new contract

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = ({ drv, peers, serviceEvents }) => {
       sender,
       recipient,
       recipientAddress,
-      contract: 'DRV100',
+      contract: 'DRV200',
       usdValue,
       drvValue,
       isDrv: true
