8d44c2d3e147a551215932531c76b01bec259046
exactchange <benny@exactchange.network>
2022-09-29 18:14:07 -0700

    on-chain magnet uri for text

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  *
  */
 
-const { capitalizeSlug, generateUUID } = require('cryptography-utilities');
+const { capitalizeSlug } = require('cryptography-utilities');
 
 module.exports = ({ drv, peers, serviceEvents }) => {
 
@@ -38,17 +38,61 @@ module.exports = ({ drv, peers, serviceEvents }) => {
     drvValue = 'data:drv/text;text,Hello world!'
   }) => {
     if (drvValue.substring(0, 9) === 'data:drv/') {
-      const encoding = drvValue === 'text' ? 'text' : 'json';
+      const encoding = drvValue.substring(9, 13) === 'text' ? 'text' : 'json';
       const drvContent = drvValue.split(`;${encoding}`);
       const contentType = drvContent[0].replace(/data:drv\//, '');
 
-      // TODO: Validate and cache file
-      // const content = encoding === 'text'
-      //   ? drvValue.split(/data\:drv\/.*;text,/)
-      //   : drvValue.split(/data\:drv\/.*;json,/);
+      const content = encoding === 'text'
+        ? drvValue.split(/data\:drv\/.*;text,/)[1]
+        : drvValue.split(/data\:drv\/.*;json,/)[1];
 
-      // eslint-disable-next-line no-param-reassign
-      drvValue = `::magnet=?xt=urn:drv/${contentType}:${generateUUID()}&dn=${capitalizeSlug(contentType)}`;
+      if (encoding === 'text') {
+
+        /*
+         * Save text records directly on-chain
+         */
+
+        // eslint-disable-next-line no-param-reassign
+        drvValue = `::magnet=?xt=urn:drv/text&dn=${encodeURIComponent(content)}&ts=${Date.now()}`;
+      } else {
+
+        /*
+         * Cache JSON records in DSS
+         */
+
+        const file = await drv.onHttpPost(
+          {
+            method: 'store',
+            body: {
+              content
+            },
+            route: {
+              path: '/'
+            },
+            path: 'store'
+          },
+          {
+            status: code => ({
+              end: () => ({
+                error: {
+                  code,
+                  message: '<DRV200> Contract error (POST).'
+                }
+              })
+            }),
+            send: body => ({
+              status: 200,
+              success: true,
+              data: body
+            })
+          }
+        );
+
+        const fileName = file.split('/')[1];
+
+        // eslint-disable-next-line no-param-reassign
+        drvValue = `::magnet=?xt=urn:drv/${contentType}:${fileName}&dn=${capitalizeSlug(contentType)}`;
+      }
     }
 
     /*
